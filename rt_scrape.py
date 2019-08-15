0b15b0dfe517a592b46ca3ef43d086f6ff04ff13
Nedeljko Vignjevic <vignjevic.nedeljko1999@gmail.com>
2019-08-15 12:45:29 +0200

    Adding rt_scrape python script

---
+++ rt_scrape.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+
+def scrape(url):
+    # Getting the html code
+    source = get(url).text
+
+    # Finding the list of the movies and parsing
+    soup = BeautifulSoup(source, "lxml")
+    div = soup.find("div", class_="panel-body content_body allow-overflow")
+    table = div.find("table", class_="table")
+    index = 1
+    for tr in table.find_all("tr"):
+        a = str(tr.find("a", class_="unstyled articleLink"))
+        try:
+            title = a.split(">")[1].split("</")[0].strip()
+            print(f"{index}. {title}")
+            index += 1
+        except:
+            pass
+    print()
+
+
+if __name__ == "__main__":
+
+    urls = [
+        "https://www.rottentomatoes.com/top/bestofrt/?year=2016",
+        "https://www.rottentomatoes.com/top/bestofrt/?year=2017",
+        "https://www.rottentomatoes.com/top/bestofrt/?year=2018",
+    ]
+
+    for url in urls:
+        scrape(url)
